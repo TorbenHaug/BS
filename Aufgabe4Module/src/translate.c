4782f113300c34b61ca712ccb7a49e40bd1634a7
torbenhaug <claustorbenhaug@hotmail.de>
2014-12-06 22:40:43 +0100

    caeser algorythm for usage include caeser.h call char caeser(char moved_char, int shift)

--- Aufgabe4Module/src/translate.c
+++ Aufgabe4Module/src/translate.c
@@ -3,17 +3,3 @@
  * There will be added two devices to your dev folder: /dev/trans0, /dev/trans1
  * trans0 will encrypt a given chararray and trans0 will decrypt it.
  */
-
-
-/**
- * Char de-/encrypter.
- * This function will move one char on the filetable by a given number.
- * The chars have to be in the following range: ['A'-'Z'] | [' '] | ['a'-'z'].
- * All other chars will only reached througt this function.
- * @move_char - char to be de-/encrypted
- * @shift	  - number of letters wich the char will be move, + means right, - means left
- * 				caeser('b',1) -> 'c', caeser('b',-1) -> 'a', caeser('ß',-1) -> 'ß'
- */
-static char caeser(char move_char, int shift){
-
-}
